a623761a1e0bd30b885f5d911d86056acf020ea5
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-15 08:40:12 +0100

    Custom validation with html and css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Form from "./components/form/Form";
+
+function App() {
+    return (
+        <div className="app">
+            <Form/>
+        </div>
+    );
+}
+
+export default App;
